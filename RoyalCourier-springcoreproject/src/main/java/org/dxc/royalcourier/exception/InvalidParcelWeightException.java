a6f860433d01a539eccdf26708ed24be4d398ad7
asnan986 <asnanmh986.amh@gmail.com>
2020-07-28 18:43:01 +0530

    spring commit

---
+++ RoyalCourier-springcoreproject/src/main/java/org/dxc/royalcourier/exception/InvalidParcelWeightException.java
@@ -0,0 +1,19 @@
+package org.dxc.royalcourier.exception;
+
+public class InvalidParcelWeightException extends Exception {
+
+	public InvalidParcelWeightException() {
+		super();
+		System.out.println();
+		// TODO Auto-generated constructor stub
+	}
+	public String toString() {
+		
+		return "Invalid Parcel Weight";
+	}
+	
+	
+	
+	
+
+}
